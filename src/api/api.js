e182a046edd1c21b206377368b4cdc37e25cf0e6
Yuriy Efimov <79786411+Yuriyfm@users.noreply.github.com>
2021-12-21 18:22:58 +0700

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,6 @@
 import * as axios from "axios";
 
-let token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90e' +
-    'XBlIjoiYWNjZXNzIiwiZXhwIjoxNjQyMjExMDI4LCJpYXQiOjE2Mzk2MTk' +
-    'wMjgsImp0aSI6ImRiYzVmMjgzOGRlMDQwMjU4NGQ2MjVhYmVhZjU1NWNkIi' +
-    'widXNlcl9pZCI6MX0.KNybOMH_BEkdz-hU45EORMLzo6kT6iU4uszjvsvEblk'
+let token = ''
 
 const config = {
     headers: {
